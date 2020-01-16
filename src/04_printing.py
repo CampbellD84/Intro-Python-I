b0dc634a4d7bb9500c3354031ca6b92049908ff8
Douglas Campbell <dacampbelltbone@gmail.com>
2020-01-16 18:52:28 -0500

    Fixed decimal rounding issue and changed dict to tuple in 04_printing.py

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,8 +11,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-print('x is %(x)d, y is %(y)f, z is "%(z)s"' %
-      {"x": 10, "y": round(2.24552, 2), "z": "I like turtles!"})
+print('x is %i, y is %4.2f, z is "%s"' %
+      (x, y, z))
 
 # Use the 'format' string method to print the same thing
 print('x is {0}, y is {1}, z is "{2}"'.format(x, round(y, 2), z))
